e340b3b305448a13d6366a713ebfa1da18a15c5b
Jasmine Beck <109075752+jazzyxbee@users.noreply.github.com>
2023-09-22 02:39:07 +1200

    Update stepper_motor_code.ino
    finished

--- stepper_motor_code.ino
+++ stepper_motor_code.ino
@@ -1,79 +1,64 @@
 #include <Stepper.h>
 
-const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
-// for your motor
-const int cameraTriggerPin = 12;  // Adjust this based on setup
+const int stepsPerRevolution = 2048;  // change this to fit the number of steps per revolution
+const int cameraTriggerPin = 12;  // Out of the way
 
 // Create a Stepper object
-Stepper myStepper(stepsPerRevolution, 2, 3, 4, 5);  // Specify the motor pins
-Stepper myStepperBackwards(stepsPerRevolution, 5, 2, 3, 4);
+Stepper myStepper(stepsPerRevolution, 2, 3, 4, 5);  // Forwards Direction
+Stepper myStepperBackwards(stepsPerRevolution, 5, 2, 3, 4); // Backwards Direction
 
 void setup() {
-	// set the speed at 60 rpm:
-	myStepper.setSpeed(50);
-	// initialize the serial port:
-	Serial.begin(9600);
+	myStepperBackwards.setSpeed(10);
+	Serial.begin(9600); // initialize the serial port
 	pinMode(cameraTriggerPin, OUTPUT);
-
 }
 
 void loop() {
-	// step one revolution  in one direction:
-	Serial.println("anticlockwise");
-	myStepper.step(stepsPerRevolution);
-	delay(100);
-	
-	Serial.println("clockwise");
-	myStepperBackwards.step(stepsPerRevolution);
-	delay(100);
+  float stepSize = 0.0;
+  float numSteps = 0.0; // 1 mm per rotation of screw
 
-	// Define the number of steps per revolution for stepper motor
-	//  const int stepsPerRevolution = 200;  // Adjust this based on  motor
+  // Ask the user for the step size and number of steps
+   Serial.println("Enter step size (in mm): ");
+   while (!Serial.available()) {}  // Wait for user input
+   stepSize = Serial.parseFloat(); // Read user input
+   while (Serial.available()) Serial.read(); // Clear input buffer
 
-	// Define the camera trigger pin
-	const int cameraTriggerPin = 12;  // Adjust this based on setup
+  Serial.println("Enter number of steps: ");
+  while (!Serial.available()) {}  // Wait for user input
+  numSteps = Serial.parseFloat(); // Read user input
+  while (Serial.available()) Serial.read(); // Clear input buffer
 	
+  // Move the stepper motor based on user input
+  moveMotor(stepSize, numSteps);
+
+  // Trigger the camera
+  // triggerCamera();
+
+  delay(1000);
 }
 
-//  void loop2() {
-//    int stepSize;
-//    int numSteps;
 
-// 1 mm per rotation of screw
-//
-//    // Ask the user for the step size and number of steps
-//    // Serial.println("Enter step size (in mm): ");
-//    // while (!Serial.available()) {}  // Wait for user input
-//    stepSize = 10;
-//
-//    // Serial.println("Enter number of steps: ");
-//    // while (!Serial.available()) {}  // Wait for user input
-//    numSteps = 500;
-//
-//    // Move the stepper motor based on user input
-//    moveMotor(stepSize, numSteps);
-//
-//    // Trigger the camera
-//    triggerCamera();
-//
-//    // Delay for some time (adjust as needed)
-//    delay(800);
-//  }
-
-void moveMotor(int stepSize, int numSteps) {
+void moveMotor(float stepSize, float numSteps) {
+  float steps_mm = 256.0;
 	// Calculate the number of steps needed for the specified distance
-	int stepsToMove = (numSteps * stepsPerRevolution) / (stepSize * 360.0);
+  for(int i = 0; i < numSteps; i++){
+    float stepsToMove = stepSize * steps_mm;
+    Serial.println(stepsToMove);
 
 	// Rotate the stepper motor
-	myStepper.step(stepsToMove);
+	  myStepperBackwards.step(stepsToMove);
+    delay(1000);
+  }
+  Serial.println("it move");
 }
 
 void triggerCamera() {
 	// Send a 5V electric pulse to trigger the camera
 	digitalWrite(cameraTriggerPin, HIGH);
-	delay(10);  // Adjust the delay as needed
+	delay(10);  
 	digitalWrite(cameraTriggerPin, LOW);
-	// step one revolution in the other direction:
+	
+  // step one revolution in the other direction:
 	Serial.println("counterclockwise");
 	myStepper.step(stepsPerRevolution);
 	delay(1000);
